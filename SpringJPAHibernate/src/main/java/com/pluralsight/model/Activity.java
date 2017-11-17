f96c796bc15448a64d31cd833cf0d12191dca587
Arjun Panicker <arjunpanicker@users.noreply.github.com>
2017-11-17 18:09:03 +0530

    Configured JPA to Spring

--- SpringJPAHibernate/src/main/java/com/pluralsight/model/Activity.java
+++ SpringJPAHibernate/src/main/java/com/pluralsight/model/Activity.java
@@ -1,7 +1,14 @@
 package com.pluralsight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Activity {
 
+	@Id
+	private Long id;
+	
 	private String desc;
 
 	public String getDesc() {
